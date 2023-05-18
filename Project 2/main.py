ba4a432fe7bc708b21f6ee186baff313b0290ac9
Cecilie Knudsen <cecilikn@stud.ntnu.no>
2023-05-19 09:33:50 +1000

    Completed jaccard similiarity

--- Project 2/main.py
+++ Project 2/main.py
@@ -24,6 +24,27 @@ def construct_graph(file=None, list =None):
             G.add_edge(int(line[0]), int(line[1]))
     return G
 
+
+
+
+def validation_set_pairs(positive_file, negative_file):
+
+    data_set = []
+    labels = []
+    with open(positive_file, 'r') as file:
+        for line in file.readlines():
+            line = line.strip().split(" ")
+            data_set.append((line[0], line[1]))
+            labels.append(1)
+    with open(negative_file, 'r') as file:
+        for line in file.readlines():
+            line = line.strip().split(" ")
+            data_set.append((line[0], line[1]))
+            labels.append(0)
+    # print("This is the data set: " + str(data_set))
+    # print("These are the labels: " + str(labels))
+    return data_set, labels
+
 def get_neighbors(graph):
     """
     Finding the neighbors
@@ -35,10 +56,59 @@ def get_neighbors(graph):
         neighbors_dict[node] = set(graph[node])
     return neighbors_dict
 
-def compute_jaccard():
-    return
+def compute_jaccard(node_pairs, neighbors):
+    """
+    Formula for computing the Jaccard similiarity
+    :param node1:
+    :param node2:
+    :param neighbors:
+    :return:
+    """
 
-def read_training_data(file):
-    data = []
+    score_dict = dict()
+    for pair in node_pairs:
+        author1 = int(pair[0])
+        author2 = int(pair[1])
+
+        score = len(neighbors[author1].intersection(neighbors[author2]))
+        score = float(score)/len(neighbors[author1].union(neighbors[author2]))
+        score_dict[str(author1) + " and " + str(author2)] = score
+
+    # Checking how many values over zero we have
+    values_over_zero = 0
+    for values in score_dict.values():
+        if values >0:
+            values_over_zero+=1
+    print("This is the Jaccard score: " + str(score_dict))
+    print("The number of score over zero: " + str(values_over_zero))
+    return score_dict
+
+def sort_dict(results, num):
+    """
+    Function to sort the top results
+    :param results: needs sorting
+    :param num: top values
+    :return: The extrracted top values
+    """
+    sorted_bc = dict(reversed(sorted(results.items(), key=lambda c: c[1])))
+    return list(sorted_bc.keys())[:num]
 
 
+
+def main():
+    G_training = construct_graph("training.txt")
+    neighbors = get_neighbors(G_training)
+    validation_pairs, _ = validation_set_pairs(
+        "val_positive.txt", "val_negative.txt")
+
+    # Computing the jaccard similarity
+    jaccard_score = compute_jaccard(validation_pairs, neighbors)
+    top_100_values = sort_dict(jaccard_score, 100)
+    print("The top 100 pairs: " + str(top_100_values))
+
+
+
+
+
+if __name__ == '__main__':
+    main()

03278c3227b81b09a023d4ce9bd556a34d241440
Cecilie Knudsen <cecilikn@stud.ntnu.no>
2023-05-22 10:10:09 +1000

    Code cleanup and finalizing

--- Project 2/link_prediction_project.py
+++ Project 2/link_prediction_project.py
@@ -6,7 +6,7 @@ Link Prediction
 import networkx as nx
 
 
-def construct_graph(file=None, list=None):
+def construct_graph(file):
     """
     Function for constructing the graph
     Used the dataset provided
@@ -14,37 +14,41 @@ def construct_graph(file=None, list=None):
     :return: The graph
     """
     G = nx.Graph()
-    # Based on if the input is a list or a file we construct the graph
-    if list:
-        G.add_edges_from(list)
-    elif file:
-        file = open(file, 'r')
-        for line in file.readlines():
-            line = line.strip().split(" ")
-            G.add_edge(line[0], line[1])
+    # Construct the graph from the given file
+    file = open(file, 'r')
+    for line in file.readlines():
+        line = line.strip().split(" ")
+        G.add_edge(line[0], line[1])
     return G
 
 
 def validation_set_pairs(positive_file, negative_file):
+    """
+    Method for creating the validation pairs that are
+    used for the training of the algorithm
+    :param positive_file: positive edges (exist)
+    :param negative_file: negative edges (does not exist)
+    :return: list of the pairs
+    """
     data_set = []
-    labels = []
     with open(positive_file, 'r') as file:
         for line in file.readlines():
             line = line.strip().split(" ")
             data_set.append((line[0], line[1]))
-            labels.append(1)
     with open(negative_file, 'r') as file:
         for line in file.readlines():
             line = line.strip().split(" ")
             data_set.append((line[0], line[1]))
-            labels.append(0)
-    # print("This is the data set: " + str(data_set))
-    # print("These are the labels: " + str(labels))
-    return data_set, labels
+    return data_set
 
 
-# TODO: mulig vi ikke trenger denne definisjonen
 def set_pairs(file):
+    """
+    Method for creating pairs
+    Used for the test data
+    :param file: file we want to pair
+    :return: list of the pairs
+    """
     data_set = []
     with open(file, 'r') as file:
         for line in file.readlines():
@@ -57,6 +61,8 @@ def get_neighbors(graph):
     """
     Finding the neighbors
     Necessary for the neighborhood-methods
+    :param graph: graph we want pairs from
+    :return: dictonary with pairs
     """
     neighbors_dict = dict()
     # Method for finding all the neighbors
@@ -78,11 +84,11 @@ def reverse_pair(pair):
 
 def compute_jaccard(node_pairs, neighbors):
     """
-    Method for computing the Jaccard distance
+    Method for computing the Jaccard Similiarity
 
-    :param node_pairs: pairs of nodes to compute for
-    :param neighbors: the dict of neighbors
-    :return: dict of jaccard scores
+    :param node_pairs: Pairs of nodes to compute
+    :param neighbors: The dictonary of neighbors from graph
+    :return: Dictonary of jaccard scores
     """
 
     score_dict = dict()
@@ -103,6 +109,13 @@ def compute_jaccard(node_pairs, neighbors):
 
 
 def jaccard_with_library(G, node_pairs):
+    """
+    Computing Jaccard Similiarty utilizing
+    the NetworkX library
+    :param G: Graph to compute for
+    :param node_pairs: node pairs used
+    :return: The dictonary of scores
+    """
     score_dict = dict()
     jaccard = nx.jaccard_coefficient(G, node_pairs)
     for u, v, p in jaccard:
@@ -111,6 +124,13 @@ def jaccard_with_library(G, node_pairs):
 
 
 def compute_adamic_adar(G, edges):
+    """
+    Computing Adamic Adar utilizing
+    the NetworkX library
+    :param G: Graph to compute for
+    :param edges: The relevant edges
+    :return: The dictonary of scores
+    """
     score_dict = dict()
     adamic_adar = nx.adamic_adar_index(G, edges)
     for u, v, p in adamic_adar:
@@ -119,6 +139,13 @@ def compute_adamic_adar(G, edges):
 
 
 def compute_preferential_attachement(G, edges):
+    """
+        Computing Preferential Attachment utilizing
+        the NetworkX library
+        :param G: Graph to compute for
+        :param edges: The relevant edges
+        :return: The dictonary of scores
+    """
     score_dict = dict()
     preferential_attachement = nx.preferential_attachment(G, edges)
     for u, v, p in preferential_attachement:
@@ -127,6 +154,13 @@ def compute_preferential_attachement(G, edges):
 
 
 def compute_resource_allocation_index(G, edges):
+    """
+        Computing Resource Allocation Index utilizing
+        the NetworkX library
+        :param G: Graph to compute for
+        :param edges: The relevant edges
+        :return: The dictonary of scores
+        """
     score_dict = dict()
     resource_allocation_index = nx.resource_allocation_index(G, edges)
     for u, v, p in resource_allocation_index:
@@ -135,6 +169,14 @@ def compute_resource_allocation_index(G, edges):
 
 
 def compute_common_neighbor_centrality(G, edges, alpha=0.8):
+    """
+        Computing Common Neighbor Centrality utilizing
+        the NetworkX library
+        :param G: Graph to compute for
+        :param edges: The relevant edges
+        :param alpha: choosen Alpha value
+        :return: The dictonary of scores
+        """
     score_dict = dict()
     common_neighbor_centrality = nx.common_neighbor_centrality(G, edges, alpha)
     for u, v, p in common_neighbor_centrality:
@@ -143,6 +185,15 @@ def compute_common_neighbor_centrality(G, edges, alpha=0.8):
 
 
 def calculate_score(results, positive, measure_method):
+    """
+    Method for computing the proximity score
+    used to evaluate which algorithm is the
+    best for link prediction
+    :param results: Results to calculate for
+    :param positive: The positive edges
+    :param measure_method: Which algorithm used
+    :return: The proximity score
+    """
     score = 0
     positive_pairs = dict.fromkeys(pair[0] + " & " + str(pair[1]) for pair in positive.edges())
     for pair in results:
@@ -150,7 +201,7 @@ def calculate_score(results, positive, measure_method):
         if pair[0] in positive_pairs or reverse_the_pair in positive_pairs:
             score += 1
     accuracy_score = (score / len(positive_pairs)) * 100
-    print("This is the accuracy score for %s: %f" %(measure_method,  accuracy_score ) + "%")
+    print("This is the accuracy score for %s: %f" % (measure_method, accuracy_score) + "%")
     return accuracy_score
 
 
@@ -158,24 +209,31 @@ def sort_dict(results, num):
     """
     Function to sort the top results
     :param results: needs sorting
-    :param num: top values
+    :param num: top values needed
     :return: The extracted top values in a list
     """
     return sorted(results.items(), key=lambda c: c[1], reverse=True)[:num]
 
 
 def format_fix(list):
+    """
+    Method for fixing the format
+    so it is in the desired way
+    for the results file
+    :param list: Result list written to file
+    :return: List in the wanted format
+    """
     results = []
     for pair in list:
-        results.append(pair[0].replace(" &", "")+ "\n")
+        results.append(pair[0].replace(" &", "") + "\n")
     return results
 
 
-def write_to_file(results,measure, value="w"):
+def write_to_file(results, measure, value="w"):
     """
     Function to write to file
     :param results: written to file
-    :param value: write or append
+    :param value: write or append, default write
     :return: none
     """
     f = open("results.txt", value)
@@ -184,47 +242,38 @@ def write_to_file(results,measure, value="w"):
     print("The results is written to file with measurement %s, good job! " % (measure))
 
 
-
 def main():
     G_training = construct_graph("training.txt")
     G_positive = construct_graph("val_positive.txt")
-    G_test = construct_graph("test.txt")
     test_data = set_pairs("test.txt")
 
     neighbors = get_neighbors(G_training)
-    validation_pairs, _ = validation_set_pairs(
-        "val_positive.txt", "val_negative.txt")
+    validation_pairs= validation_set_pairs("val_positive.txt", "val_negative.txt")
 
     # Computing the Jaccard Similarity
     jaccard = compute_jaccard(validation_pairs, neighbors)
     top_100_values = sort_dict(jaccard, 100)
     jaccard_score = calculate_score(top_100_values, G_positive, "Jaccard Similarity")
 
-
     # Computing the Jaccard simialirty using networkx
     jaccard_nx = jaccard_with_library(G_training, validation_pairs)
     top_100_jaccard_nx = sort_dict(jaccard_nx, 100)
-
     jaccard_score_nx = calculate_score(top_100_jaccard_nx, G_positive, "Jaccard Similarity Networkx")
-
 
     # Computing Adamic Adar
     adamic_adar = compute_adamic_adar(G_training, validation_pairs)
     top_100_adamic = sort_dict(adamic_adar, 100)
     adamic_score = calculate_score(top_100_adamic, G_positive, "Adamic Adar")
 
-
     # Computing Preferential Attachement
     preferential_attachement = compute_preferential_attachement(G_training, validation_pairs)
     top_100_pa = sort_dict(preferential_attachement, 100)
     pa_score = calculate_score(top_100_pa, G_positive, "Preferential Attachement")
 
-
     # Computing Resource Allocation Index
     resource_allocation_index = compute_resource_allocation_index(G_training, validation_pairs)
     top_100_rai = sort_dict(resource_allocation_index, 100)
     rai_score = calculate_score(top_100_rai, G_positive, "Resource Allocation Index")
-
 
     # Computing Common Neighbor Centrality
     common_neigbor_centrality = compute_resource_allocation_index(G_training, validation_pairs)
@@ -236,12 +285,7 @@ def main():
     resource_allocation_index_test = compute_resource_allocation_index(G_training, test_data)
     top_100_test = sort_dict(resource_allocation_index_test, 100)
     final_results = format_fix(top_100_test)
-    print(final_results)
     write_to_file(final_results, "Resource Allocation Index")
-
-
-
-
 
 
 if __name__ == '__main__':
